1707f52c39417352c4fbffab32e7087b65a16457
Gerardo Marx <gmarx_cc@itmorelia.edu.mx>
2023-09-02 02:32:18 +0200

    module ready

--- main.py
+++ main.py
@@ -2,6 +2,21 @@
 
 TEMP_PATH="/sys/class/thermal/thermal_zone0/"
 
-fo = open(TEMP_PATH + "temp","r")
-temp=fo.read()
-print(temp)
+def readTemp():
+    fo = open(TEMP_PATH + "temp", "r")
+    temp = fo.read()
+    return int(temp)
+
+
+def average(n):
+    sum = 0
+    for i in range(n):
+        sum = sum + readTemp()
+    average = sum/n
+    return average*0.001
+
+
+#value = average(4)
+#print(value)
+# Check as module 
+# import main as gm
